e150e3eec63fe19d80de0036ad0f35f076ad5764
AakibAnsari <aakiba6619@gmail.com>
2023-11-06 20:06:47 +0545

    first deploy

---
+++ src/database/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const configOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const connectToDB = async () => {
+  const connectionUrl =
+    "mongodb+srv://ansariaakib310:L5Vo4XMuco5zwrrR@cms.wt4ftza.mongodb.net/";
+
+  mongoose
+    .connect(connectionUrl, configOptions)
+    .then(() => console.log("Ecommerce database connected successfully!"))
+    .catch((err) =>
+      console.log(`Getting Error from DB connection ${err.message}`)
+    );
+};
+
+export default connectToDB;
